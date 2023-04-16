60aa5854b8bf5f719a7e3dc3f100f569a8d3925e
freddiejones <fredjjones1700@gmail.com>
2023-04-16 14:27:30 -0400

    Updated scrawler with functions for all three

--- Crawler.py
+++ Crawler.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 # Packages and Imports
 import tweepy
 import pandas as pd
@@ -24,12 +18,9 @@ import matplotlib.pyplot as plt # For plotting graph
 import pickle # Handling output?
 
 
-# In[7]:
-
-
 # Twitter dev account login for access keys/tokens/secrets - Example 1 Cookbook
-    
 # Elevated account access
+
 CONSUMER_KEY = '12Cyj4UGAlzhqicly5CCQgpK0'
 CONSUMER_SECRET = 'UpyuspnNzo0YTpXWy7h9qLJAZSfCXsDmQlQGQpk6TMCdPwooH8'
 OAUTH_TOKEN = '3304948091-0iu8Qs2xOlmP99dyRB4STJlxYIz09GTgZqaUDN4'
@@ -41,11 +32,6 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 # create the API object
 api = tweepy.API(auth)
-
-
-# In[8]:
-
-
 # Keywords - word bank for tweet scraping
 
 tweets = []
@@ -60,10 +46,7 @@ artist_keywords = ["weeknd", "Weeknd", "Red Hot Chili Peppers",
             "DeAndre Cortez Way", "Abel Makkonen Tesfaye"]
 
 
-# In[9]:
-
-
-def weeknd_tweets(keyword):
+def weeknd_tweets():
     weeknd_tweets = []
     weeknd_tweet_data = pd.DataFrame(columns=['tweet_id', 'Username', 'text', 'Artist', 'created_at'])
     num_tweets = 50
@@ -82,20 +65,39 @@ def weeknd_tweets(keyword):
     return weeknd_tweet_data
 
 
-# In[10]:
+def chili_pepper_tweets():
+    chili_pepper_tweets = []
+    chili_pepper_tweet_data = pd.DataFrame(columns=['tweet_id', 'Username', 'text', 'Artist', 'created_at'])
+    num_tweets = 10
+    
+    for keyword in chili_pepper_keywords:
+        for cp_tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang='en').items(num_tweets):
+            chili_pepper_tweets.append(cp_tweet)
+    
+    for cp_tweet in chili_pepper_tweets:
+        chili_pepper_tweet_data = chili_pepper_tweet_data.append({'tweet_id': cp_tweet.id,
+                                                      'Username': cp_tweet.user.screen_name,
+                                                      'text': cp_tweet.text,
+                                                      'Artist': "Red Hot Chili Peppers",
+                                                      'created_at': cp_tweet.created_at},
+                                                     ignore_index=True)
+    return chili_pepper_tweet_data
 
 
-print(weeknd_tweets("Abel Makkonen Tesfaye"))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+def soulja_boy_tweets():
+    soulja_boy_tweets = []
+    soulja_boy_tweet_data = pd.DataFrame(columns=['tweet_id', 'Username', 'text', 'Artist', 'created_at'])
+    num_tweets = 10
+    
+    for keyword in soulja_boy_keywords:
+        for sb_tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang='en').items(num_tweets):
+            soulja_boy_tweets.append(sb_tweet)
+    
+    for sb_tweet in soulja_boy_tweets:
+        soulja_boy_tweet_data = soulja_boy_tweet_data.append({'tweet_id': sb_tweet.id,
+                                                      'Username': sb_tweet.user.screen_name,
+                                                      'text': sb_tweet.text,
+                                                      'Artist': "Soulja Boy",
+                                                      'created_at': sb_tweet.created_at},
+                                                     ignore_index=True)
+    return soulja_boy_tweet_data
