96ae2a84ab80a390bb086f1d7d6417d1eca000e8
freddiejones <fredjjones1700@gmail.com>
2023-04-17 19:42:17 -0400

    Added clean_tweets function for text preprocessing

--- Crawler.py
+++ Crawler.py
@@ -9,6 +9,8 @@ import matplotlib
 import numpy
 import sys
 import time
+import re
+import preprocessor as p
 from functools import partial
 from sys import maxsize as maxint
 from urllib.error import URLError
@@ -30,19 +32,42 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 # create the API object
 api = tweepy.API(auth)
-# Keywords - word bank for tweet scraping
-tweets = []
 
+# Keywords for tweets mentioning the Weeknd, Red Hot Chili Peppers, and Soulja Boy
 weeknd_keywords = ["Weeknd", "Abel Makkonen Tesfaye"]
 chili_pepper_keywords = ["red hot chili peppers", "chili peppers", "rhcp"]
 soulja_boy_keywords = ["DeAndre Cortez Way", "Soulja Boy"]
 
-artist_keywords = ["weeknd", "Weeknd", "Red Hot Chili Peppers", 
-            "red hot chili peppers", "the weeknd", "the Weeknd",
-            "chili peppers", "Soulja Boy", "soulja boy", 
-            "DeAndre Cortez Way", "Abel Makkonen Tesfaye"]
+# Emoticons (in case of old style)
+emoticons_happy = set([
+    ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
+    ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
+    '=-3', '=3', ':-))', ":'-)", ":')", ':*', ':^*', '>:P', ':-P', ':P', 'X-P',
+    'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)',
+    '<3'
+    ])
+emoticons_sad = set([
+    ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
+    ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
+    ':c', ':{', '>:\\', ';('
+    ])
+emoticons = emoticons_happy.union(emoticons_sad)
+# Emojis
+emoji_pattern = re.compile("["
+         u"\U0001F600-\U0001F64F"  # emoticons
+         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+         u"\U0001F680-\U0001F6FF"  # transport & map symbols
+         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+         u"\U00002702-\U000027B0"
+         u"\U000024C2-\U0001F251"
+         "]+", flags=re.UNICODE)
 
+# Clean tweets
+def clean_tweets(tweet):
+    clean_tweet = p.clean(tweet)
+    return clean_tweet
 
+# Crawl tweets regarding The Weeknd and store to dataframe
 def weeknd_tweets(output_file): 
     weeknd_tweets = []
     weeknd_tweet_data = pd.DataFrame(columns=['tweet_id', 'Username', 'text', 'Artist', 'created_at'])
@@ -55,14 +80,15 @@ def weeknd_tweets(output_file):
                 weeknd_tweets.append(tweet)
     
     for tweet in weeknd_tweets:
-        weeknd_tweet_data = weeknd_tweet_data.append({'tweet_id': tweet.id,
+        weeknd_tweet_data = weeknd_tweet_data.append({'tweet_id': tweet.id, # Dataframe columns being filled
                                                       'Username': tweet.user.screen_name,
                                                       'text': tweet.full_text,
                                                       'Artist': "The Weeknd",
                                                       'created_at': tweet.created_at},
                                                      ignore_index=True)
-    
     # Print the dataframe to a new file
+    weeknd_tweet_data.drop_duplicates()
+    weeknd_tweet_data['text'] = weeknd_tweet_data['text'].apply(clean_tweets)
     weeknd_tweet_data.to_csv(output_file, index=False)
     
     return weeknd_tweet_data
@@ -80,11 +106,13 @@ def classify_sentiment(text):
 def chili_pepper_tweets(output_file):
     chili_pepper_tweets = []
     chili_pepper_tweet_data = pd.DataFrame(columns=['tweet_id', 'Username', 'text', 'Artist', 'created_at'])
-    num_tweets = 10
+    num_tweets = 50
     
     for keyword in chili_pepper_keywords:
         for cp_tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang='en').items(num_tweets):
-            chili_pepper_tweets.append(cp_tweet)
+            # Filter out retweets
+            if not hasattr(cp_tweet, 'retweeted_status'):
+                chili_pepper_tweets.append(cp_tweet)
     
     for cp_tweet in chili_pepper_tweets:
         chili_pepper_tweet_data = chili_pepper_tweet_data.append({'tweet_id': cp_tweet.id,
@@ -93,6 +121,8 @@ def chili_pepper_tweets(output_file):
                                                       'Artist': "Red Hot Chili Peppers",
                                                       'created_at': cp_tweet.created_at},
                                                      ignore_index=True)
+    chili_pepper_tweet_data.drop_duplicates()
+    chili_pepper_tweet_data['text'] = chili_pepper_tweet_data['text'].apply(clean_tweets)
     chili_pepper_tweet_data.to_csv(output_file, index=False)
     return chili_pepper_tweet_data
 
@@ -104,7 +134,9 @@ def soulja_boy_tweets(output_file):
     
     for keyword in soulja_boy_keywords:
         for sb_tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang='en').items(num_tweets):
-            soulja_boy_tweets.append(sb_tweet)
+            # Filter out retweets
+            if not hasattr(sb_tweet, 'retweeted_status'):
+                soulja_boy_tweets.append(sb_tweet)
     
     for sb_tweet in soulja_boy_tweets:
         soulja_boy_tweet_data = soulja_boy_tweet_data.append({'tweet_id': sb_tweet.id,
@@ -113,6 +145,8 @@ def soulja_boy_tweets(output_file):
                                                       'Artist': "Soulja Boy",
                                                       'created_at': sb_tweet.created_at},
                                                      ignore_index=True)
+    soulja_boy_tweet_data.drop_duplicates()
+    soulja_boy_tweet_data['text'] = soulja_boy_tweet_data['text'].apply(clean_tweets)    
     soulja_boy_tweet_data.to_csv(output_file, index=False)
     return soulja_boy_tweet_data
 
@@ -137,3 +171,5 @@ def sentiment_analysis(dataframeFile):
 if __name__ == "__main__":
     weeknd_tweets('weekendOutput.csv')
     sentiment_analysis('weekendOutput.csv')
+    chili_pepper_tweets('chiliPepperOutput.csv')
+    sentiment_analysis('chiliPepperOutput.csv')
